9c67a575df94cf0cb6a449ced503e2f00d5bbf02
2GMon <2gmon.t@gmail.com>
2012-07-03 16:28:34 +0900

    GPL v3適用した(つもり)
    なにか間違ってないか不安

--- src/jp/ddo/t2gmon/httpshutter/HTTPshutterActivity.java
+++ src/jp/ddo/t2gmon/httpshutter/HTTPshutterActivity.java
@@ -1,3 +1,21 @@
+/*
+ * HttpShutter - Take a photo via http access
+ * Copyright (C) 2012 TSUJIMOTO Takaaki
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 package jp.ddo.t2gmon.httpshutter;
 
 import java.util.List;
